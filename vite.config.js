033e8793bc07055ad04aa8dca2958d89497d9a2e
Antti Järvinen <47383357+antja0@users.noreply.github.com>
2023-05-31 22:52:27 +0300

    Add vite plugin for blade templates

---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite'
+import posthtml from 'posthtml'
+import { glob } from 'glob'
+import fs from 'fs'
+
+const components = {};
+const allFiles = glob.sync('src/**/*.*')
+allFiles.forEach(file => {
+  const filename = file.split('\\').pop().split('/').pop()
+  const start = filename.substring(0, filename.indexOf('.'));
+  const ending = filename.substring(filename.indexOf('.'));
+  if (ending === '.blade.php') {
+    components[start] = fs.readFileSync(file, 'utf8')
+  }
+});
+
+const plugin = function (tree) {
+  for (const [key, value] of Object.entries(components)) {
+    tree.match({ tag: 'x-'+key }, function (node) {
+      return value;
+    })
+  }
+
+  return tree;
+}
+
+function bladeTemplatesPlugin() {
+  return {
+    name: 'blade-templates-plugin',
+    async transformIndexHtml(html) {
+      const { html: transformedHtml } = await posthtml(plugin).process(html);
+      return transformedHtml;
+    },
+    configureServer(server) {
+      server.watcher.on('change', (filePath) => {
+        if (filePath.endsWith('.blade.php')) {
+          server.restart()
+        }
+      });
+    }
+  }
+}
+
+export default defineConfig({
+  plugins: [
+    bladeTemplatesPlugin(),
+  ]
+})
